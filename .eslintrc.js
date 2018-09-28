bf542b6e20a0132075ff7792509fb88609a61559
tomoya <tomoya.ton@gmail.com>
2018-09-29 08:02:39 +0900

    Create eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["react-app", "plugin:prettier/recommended"],
+  "plugins": ["prettier"],
+  "rules": {
+    "prettier/prettier": [
+      "error", {
+        "semi": true,
+        "singleQuote": true,
+        "trailingComma": "all",
+      },
+    ],
+  },
+}
